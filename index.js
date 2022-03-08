975c56fc112e0bdee3797d555bcdcb36df5acd2b
DazingPluto <92892528+DazingPluto@users.noreply.github.com>
2022-03-08 12:16:33 -0600

    Portfolio

---
+++ index.js
@@ -0,0 +1,17 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const person = {
+    name: "John Doe",
+    age: 39,
+    foods: [
+        'burgers',
+        'pies'
+    ],
+    car: {
+        name: 'Tesla',
+        model: 'Model S',
+        year: 2020
+    }
+}
+
+console.log(numbers);
+console.log(person.foods);
